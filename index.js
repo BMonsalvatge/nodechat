27c952031f105ca9dbc2408ff6bb7ce13c5edb9d
Bryan Monsalvatge <dreadnought93@gmail.com>
2017-08-10 23:46:37 -0400

    User Prompt
    Got the user prompt working.  However Current issue is whenever a new user joins it overwrites the existing user.

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+/*
+app.get('/chatroom.html', function(req, res){
+  res.sendFile(__dirname + '/chatroom.html');
+});
+*/
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
