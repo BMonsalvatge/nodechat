a9528acb797d51920abcbf26b0ed74582f11b711
Bryan Monsalvatge <dreadnought93@gmail.com>
2017-08-12 20:06:57 -0400

    Changes Made
    Some updates made by guy

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-/*
-app.get('/chatroom.html', function(req, res){
-  res.sendFile(__dirname + '/chatroom.html');
-});
-*/
 
 io.on('connection', function(socket){
+
+  var name = clients.indexOf(socket.id);
+
+  console.log(name, socket.id, clients);
+
+  if (!name) {
+    name = clients.length-1;
+    clients.push(socket.id);
+  }
+  
+  console.log(clients);
+
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', `<strong>User ${name}:</strong> ${msg}`);
   });
 });
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
