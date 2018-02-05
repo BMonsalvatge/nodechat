06322e68922e5fe7aadfc1a54fe0bd07796d9910
Bryan Monsalvatge <dreadnought93@gmail.com>
2018-02-05 14:32:26 -0500

    Room
    Working on making rooms work based on the express route.

--- index.js
+++ index.js
@@ -6,35 +6,39 @@ const io = require('socket.io')(http);
 app.set('view engine','ejs');
 
 // use res.render to load up an ejs view file
-
 // index page 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.render('pages/index');
-});
+  const url = req.url;
 
-io.on('connection', (socket) => {
-  // Sends message when someone joins the page
-  io.emit('system message', `A User Join the Chat`);
+  io.on('connection', (socket) => {
+    // Sends message when someone joins the page
+    io.emit('system message', `A User Join the Chat`);
+  
+    var name = "Temp User " + socket.id;
+    
+    socket.join(url);
 
-  var name = "Temp User " + socket.id;
-
-  console.log(socket.id);
-
-  // When a username is picked
-  socket.on('nickname pick', (nickname) => {
-    io.emit('system message', `${name} changed their nickname to ${nickname}`);
-    name = nickname;
-    return name;
-  });
-
-  // When a normal chat message is sent
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', `${name}: ${msg}`);
-  });
-
-  // Used for system messages that will face the client
-  socket.on('system message', (msg) => {
-    io.emit('system message', `${msg}`);
+    console.log(url);
+  
+    console.log(socket.id);
+  
+    // When a username is picked
+    socket.on('nickname pick', (nickname) => {
+      io.emit('system message', `${name} changed their nickname to ${nickname}`);
+      name = nickname;
+      return name;
+    });
+  
+    // When a normal chat message is sent
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', `${name}: ${msg}`);
+    });
+  
+    // Used for system messages that will face the client
+    socket.on('system message', (msg) => {
+      io.emit('system message', `${msg}`);
+    });
   });
 });
 
